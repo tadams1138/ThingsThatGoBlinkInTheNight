28fc0b5463d4a2cd26a35e0c75614b648e9dc5d9
Tom Adams <tadams1138@hotmail.com>
2019-08-11 20:56:56 -0400

    build toc

---
+++ build_toc.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cat > toc.html << EOF
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="icon" href="favicon.ico">
+    <link rel="stylesheet" href="styles.css">
+    <script type="text/javascript" src="scripts.js"></script>
+    <title>Table of Contents</title>
+</head>
+<body>
+<ul>
+EOF
+
+find . -name '*.html' | sort -r | while read fname; do
+    title="$(grep -P -o '(?<=<title>).*(?=</title>)' $fname)"
+cat >> toc.html << EOF
+<li><a href="$fname">$title</a></li>
+EOF
+done
+
+cat >> toc.html << EOF
+</ul>
+</body>
+</html>
+EOF
+
