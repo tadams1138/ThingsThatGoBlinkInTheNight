db27b7d32b5491199b937a57d76d05f9a833028a
Tom Adams <tadams1138@hotmail.com>
2020-04-28 14:02:42 -0400

    add google analytics

--- build_toc.sh
+++ build_toc.sh
@@ -18,6 +18,16 @@ cat >toc.html <<EOF
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-76968909-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-76968909-1');
+    </script>
+
     <meta charset="UTF-8">
     <link rel="icon" href="favicon.ico">
     <link rel="stylesheet" href="styles.css">
